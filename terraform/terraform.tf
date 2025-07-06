34759c00e89e708ade552b75c8f3df58b85611c0
Sidhant Huge <siddhanthuge@gmail.com>
2025-07-06 14:50:50 +0530

    Updated Bucket

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
  
   backend "s3" {
-    bucket = "gitops-project23"
+    bucket = "gitops-project234"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = "~> 1.6.3"
